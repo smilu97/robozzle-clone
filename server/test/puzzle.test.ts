d513bf5a7ef36f56829f8bc260b415dbf69b8d5b
smilu97 <smilup2244@gmail.com>
2021-06-18 14:31:29 +0900

    server: add puzzle test

---
+++ server/test/puzzle.test.ts
@@ -0,0 +1,22 @@
+import * as puzzle from '../src/puzzle';
+
+test('Load sample tutorial puzzle', async () => {
+  const puz = await puzzle.getPuzzleByName('tutorial');
+  expect(puz).not.toBeFalsy();
+});
+
+test('Load sample tutorial puzzle', async () => {
+  const puz = await puzzle.default();
+  expect(puz).toHaveProperty('length');
+  expect(puz[0]).toHaveProperty('name');
+});
+
+test('Convert puzzle into metadata', () => {
+  const target = {
+    name: 'test',
+    dummy: 4,
+  } as any;
+  const meta = puzzle.convertPuzzleIntoMeta(target);
+  expect(meta).toHaveProperty('name', 'test');
+  expect(meta).not.toHaveProperty('dummy');
+});
