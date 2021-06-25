c8ab90b7491f40f62eaf70260a6b3e61df4e9210
smilu97 <smilup2244@gmail.com>
2021-06-25 21:20:22 +0900

    webclient: resolve only single call op in one step

--- webclient/src/env/opStack.ts
+++ webclient/src/env/opStack.ts
@@ -33,14 +33,20 @@ export default class OpStack extends Stack<RobozzleStackItem> {
      * @returns operation which is not call, or empty, but action, or write
      */
     popOp(): RobozzleActionOperation | RobozzleWriteOperation | null {
-        this._resolveCalls();
+        const top = this.pop();
+        if (top === null) return null;
 
-        if (this.empty)
-            return null;
+        const { op } = top;
         
-        const top = this.top();
+        if (op.type === RobozzleOpTypes.call) {
+            this._resolveCall(op);
+            return null;
+        }
 
-        return top as RobozzleActionOperation | RobozzleWriteOperation | null;
+        if (op.type === RobozzleOpTypes.empty)
+            return this.popOp();
+
+        return op;
     }
 
     /**
@@ -68,41 +74,12 @@ export default class OpStack extends Stack<RobozzleStackItem> {
         return this.lastId;
     }
 
-    /**
-     * Assert the item on the top is not function call operation,
-     * or if the top-item is call operation, then resolve them until
-     * the condition is satisfied.
-     * 
-     * TODO: infinite loop may be exist
-     */
-    private _resolveCalls() {
-        while (1) {
-            const callee = this._getCalleeOnTop();
-            if (callee === null) break;
+    private _resolveCall(call: RobozzleCallOperation): void {
+        const { callee } = call;
 
-            callee.seq
-                .filter((el) => el.type !== RobozzleOpTypes.empty)
-                .reverse()
-                .forEach(this.pushOp.bind(this));
-        }
-    }
-
-    /**
-     * Get callee of the call operation on the top of stack.
-     * if stack is empty, or the top-item is not call operation,
-     * then it returns null.
-     * @returns callee | null
-     */
-    private _getCalleeOnTop() {
-        if (this.empty) return null;
-
-        const top = this.top();
-
-        if (top === null) return null;
-        if (top.op.type !== RobozzleOpTypes.call) return null;
-        
-        const { callee } = (top.op as RobozzleCallOperation);
-
-        return callee;
+        callee.seq
+            .filter((el) => el.type !== RobozzleOpTypes.empty)
+            .reverse()
+            .forEach(this.pushOp.bind(this));
     }
 }
